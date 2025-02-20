2d568e448b1c3b1fb5b1e2012a3a512b5af23842
DuanrA <arnaudscrivo.dev@proton.me>
2025-02-20 11:51:56 +0100

    simulation de telechargement de virus

---
+++ Simulation of a malicious download attack/VirusAttackSimulation.py
@@ -0,0 +1,26 @@
+import time
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+# Initialisation du clavier
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+
+# Temps d'attente pour permettre à la machine de reconnaître le clavier
+time.sleep(2)
+
+# Ouvrir un terminal (Ctrl + Alt + T sous Linux, Win + R puis "cmd" sous Windows)
+keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.T)  # Pour Linux
+# keyboard.send(Keycode.GUI, Keycode.R)  # Pour Windows
+# time.sleep(1)
+# keyboard_layout.write("cmd")
+# keyboard.send(Keycode.ENTER)
+
+# Attendre que le terminal s'ouvre
+time.sleep(1)
+
+# Simuler un téléchargement malveillant
+keyboard_layout.write("echo 'Un fichier malveillant aurait pu être téléchargé sur votre système.'")
+keyboard.send(Keycode.ENTER)
